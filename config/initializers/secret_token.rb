352cf965432214969ef0ac359437afc76fa07002
Vijay Aura <vijayendra.iitm@gmail.com>
2013-06-27 18:51:38 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geomap::Application.config.secret_token = '1cba2f28fe2ae3f1548d17c32adff7e8da47b2e19532fffa3c75d82f72c4ca28b44901b18a9afdedcd204a8b8ecdd174679a7eb7e03dbc988fc3c4ee70ff5e1c'
